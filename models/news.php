5f6166fa1a19f864d1823d327ca0574f71c02215
Sayfullayev Norbek <sayfullayev1572norbek@gmail.com>
2024-05-09 01:21:10 +0500

    ko'rishlar sonini qildik view

--- models/news.php
+++ models/news.php
@@ -14,13 +14,25 @@ function getAllNews()
 
 function getNewsById($id){
     global $pdo;
+    // id dagi ma'lumotlarni olib kelamiz
     $sql = "SELECT * FROM news WHERE status = 1 and id = $id";
     $prepare = $pdo->prepare($sql);
-    try {
+    $prepare->execute();
+    $row = $prepare->fetch();
+
+    if ($row){
+        // o'sha id dagi seen count ga har safar 1 qiymatini qo'shamiz va bazaga saqlaymiz
+        $seen_count = $row['seen_count']+1;
+        $update = $pdo->prepare("UPDATE news SET seen_count = $seen_count WHERE id = $id");
+        $update->execute();
+        // saqlangan ma'lumotni chaqirib olib return qilamiz.
+        $my_sql = $sql;
+        $prepare = $pdo->prepare($my_sql);
         $prepare->execute();
-        return $prepare->fetch();
-    }catch (PDOException $e){
-        debug($e->getMessage(), 1);
+        $result = $prepare->fetch();
+        return $result;
+    }else{
+        echo "ERROR!";
     }
 }
 
